1687892dc5d512d3d84d96a456be4151682e5b65
Federico Coppa <federicocoppa75@gmail.com>
2022-12-04 16:23:09 +0100

    dev - view time of step execution

---
+++ Machine.3D.Views/Converters/TimespanToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Machine._3D.Views.Converters
+{
+    class TimespanToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var t = (TimeSpan)value;
+
+            return t.ToString("mm\\:ss\\.fff");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
